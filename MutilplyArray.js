4a3459078807ab2f91679052e6c6903ac5927ca2
Sivaranjani-web <work.sivaranjani@gmail.com>
2024-09-27 17:30:09 +0530

    update multiplyarray file

--- MutilplyArray.js
+++ MutilplyArray.js
@@ -1,7 +1,16 @@
-const numbers = [1,2,3,4,5];
+let numbers = [1,2,3,4,5];
 numbers.forEach(myFunction)
 
 function myFunction(item, index, arr) {
   arr[index] = item * 2;
 }
+
+let arr = [1,2,3,4,5];
+let result = arr.map(x => x * 2);
+console.log(result);
+
+for (let numb of numbers) {
+console.log(numb * 2)}
+
+
 console.log(numbers)
